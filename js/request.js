721b611011ef98385bed7f5334bed8e906242a14
王强 <jack2392228720@gmail.com>
2024-01-24 19:32:17 +0800

    pref: 更改接口地址

--- js/request.js
+++ js/request.js
@@ -4,7 +4,7 @@
  * @param 说明：获取最新发布的音乐
  */
 const getNewSong = async () => {
-  const resp = await fetch('https://cloud-api-omega.vercel.app/personalized/newsong?limit=20');
+  const resp = await fetch('https://henrywang.xyz/personalized/newsong?limit=20');
   return await resp.json();
 }
 
@@ -13,7 +13,7 @@ const getNewSong = async () => {
  * @returns 对象
  */
 async function getTheDailyRecommendedSongList(){
-  const resp = await fetch(`https://cloud-api-omega.vercel.app/recommend/resource`);
+  const resp = await fetch(`https://henrywang.xyz/recommend/resource`);
   return await resp.json();
 }
 
@@ -22,7 +22,7 @@ async function getTheDailyRecommendedSongList(){
  * @returns 对象
  */
 async function getDailySongRecommendations(){
-  const resp = await fetch(`https://cloud-api-omega.vercel.app/recommend/songs`);
+  const resp = await fetch(`https://henrywang.xyz/recommend/songs`);
   return await resp.json();
 }
 
@@ -31,7 +31,7 @@ async function getDailySongRecommendations(){
  * @returns 对象
  */
 const recommendedPlaylists = async () => {
-  const resp = await fetch('https://cloud-api-omega.vercel.app/personalized?limit=9');
+  const resp = await fetch('https://henrywang.xyz/personalized?limit=9');
   return await resp.json();
 }
 
@@ -42,7 +42,7 @@ const recommendedPlaylists = async () => {
  * @param 说明：通过歌单id，获取到歌单中所有的歌曲
  */
 const getAllsongsOnThePlaylist = async (PlayListId) => {
-  const resp = await fetch(`https://cloud-api-omega.vercel.app/playlist/track/all?limit=20&id=${PlayListId}&offset=1`);
+  const resp = await fetch(`https://henrywang.xyz/playlist/track/all?limit=20&id=${PlayListId}&offset=1`);
   return await resp.json();
 }
 
@@ -53,7 +53,7 @@ const getAllsongsOnThePlaylist = async (PlayListId) => {
  * @param 说明: 使用歌单详情接口后 , 能得到的音乐的 id, 但不能得到的音乐 url, 调用此接口, 传入的音乐 id( 可多个 , 用逗号隔开 ), 可以获取对应的音乐的 url;
  */
 const getSongUrl = async (id) => {
-  const resp = await fetch(`https://cloud-api-omega.vercel.app/song/url/v1?id=${id}&level=jymaster`);
+  const resp = await fetch(`https://henrywang.xyz/song/url/v1?id=${id}&level=jymaster`);
   return await resp.json();
 }
 
@@ -64,7 +64,7 @@ const getSongUrl = async (id) => {
  * @param 说明: 歌单能看到歌单名字, 但看不到具体歌单内容 , 调用此接口 , 传入歌单 id, 可 以获取对应歌单内的所有的音乐;
  */
 const getPlaylistsDetail = async (id) => {
-  const resp = await fetch(`https://cloud-api-omega.vercel.app/playlist/detail?id=${id}`);
+  const resp = await fetch(`https://henrywang.xyz/playlist/detail?id=${id}`);
   return await resp.json();
 }
 
@@ -74,7 +74,7 @@ const getPlaylistsDetail = async (id) => {
  * @returns 返回 { success: true, message: 'ok' } 或者 { success: false, message: '亲爱的,暂无版权' }
  */
 const musicAvailability = async (id) => {
-  const resp = await fetch(`https://cloud-api-omega.vercel.app/check/music?id=${id}`);
+  const resp = await fetch(`https://henrywang.xyz/check/music?id=${id}`);
   return await resp.json();
 }
 
@@ -85,7 +85,7 @@ const musicAvailability = async (id) => {
  * @param 说明: 调用此接口 , 传入搜索关键词可以搜索该音乐 / 专辑 / 歌手 / 歌单 / 用户 , 关键词可以多个 , 以空格隔开 , 如 " 周杰伦 搁浅 "( 不需要登录 ), 可通过 /song/url 接口传入歌曲 id 获取具体的播放链接;
  */
 async function search(key){
-  const resp = await fetch(`https://cloud-api-omega.vercel.app/search?keywords=${key}`);
+  const resp = await fetch(`https://henrywang.xyz/search?keywords=${key}`);
   return await resp.json();
 } 
 
@@ -96,7 +96,7 @@ async function search(key){
  * @param 说明: 调用此接口 , 传入音乐 id(支持多个 id, 用 , 隔开), 可获得歌曲详情(dt为歌曲时长);
 */
 async function getSongDetails(id){
-  const resp = await fetch(`https://cloud-api-omega.vercel.app/song/detail?ids=${id}`);
+  const resp = await fetch(`https://henrywang.xyz/song/detail?ids=${id}`);
   return await resp.json();
 }
 
@@ -107,7 +107,7 @@ async function getSongDetails(id){
  * @param 说明: 调用此接口 , 传入音乐 id 可获得对应音乐的歌词 ( 不需要登录 );
  */
 async function getsongLyric(id){
-  const resp = await fetch(`https://cloud-api-omega.vercel.app/lyric?id=${id}`);
+  const resp = await fetch(`https://henrywang.xyz/lyric?id=${id}`);
   return await resp.json();
 }
 
@@ -118,7 +118,7 @@ async function getsongLyric(id){
  * @param 说明: 调用此接口 , 可获取默认搜索关键词;
  */
 async function searchSuggestions(keywords){
-  const resp = await fetch(`https://cloud-api-omega.vercel.app/search/suggest?keywords=${keywords}`);
+  const resp = await fetch(`https://henrywang.xyz/search/suggest?keywords=${keywords}`);
   return await resp.json();
 }
 
@@ -130,7 +130,7 @@ async function searchSuggestions(keywords){
  * @returns 包含用户信息的对象
  */
 async function mobileLogin(phone,captcha){
-  const resp = await fetch(`https://cloud-api-omega.vercel.app/login/cellphone?phone=${phone}&captcha=${captcha}`);
+  const resp = await fetch(`https://henrywang.xyz/login/cellphone?phone=${phone}&captcha=${captcha}`);
   return await resp.json();
 }
 
@@ -141,7 +141,7 @@ async function mobileLogin(phone,captcha){
  * @returns 对象
  */
 async function emailLogin(email,password){
-  const resp = await fetch(`https://cloud-api-omega.vercel.app/login?email=${email}@163.com&password=${password}`);
+  const resp = await fetch(`https://henrywang.xyz/login?email=${email}@163.com&password=${password}`);
   return await resp.json();
 }
 
@@ -151,7 +151,7 @@ async function emailLogin(email,password){
  * @param 可获取游客cookie,如果遇到其他接口未登录状态报400状态码需要验证的错误,可使用此接口获取游客cookie避免报错
  */
 async function visitorLogin(){
-  const resp = await fetch(`https://cloud-api-omega.vercel.app/register/anonimous`);
+  const resp = await fetch(`https://henrywang.xyz/register/anonimous`);
   return await resp.json();
 }
 
@@ -167,7 +167,7 @@ async function visitorLogin(){
  * @param 说明: 调用此接口 ,传入手机号码, 可发送验证码
  */
 async function sendVerificationCode(phone){
-  const resp = await fetch(`https://cloud-api-omega.vercel.app/captcha/sent?phone=${phone}`);
+  const resp = await fetch(`https://henrywang.xyz/captcha/sent?phone=${phone}`);
   return await resp.json();
 }
 
@@ -179,6 +179,6 @@ async function sendVerificationCode(phone){
  * @param 说明: 调用此接口 ,传入手机号码和验证码, 可校验验证码是否正确
 */
 async function checkCode(phone,captcha){
-  const resp = await fetch(`https://cloud-api-omega.vercel.app/captcha/verify?phone=${phone}&captcha=${captcha}`);
+  const resp = await fetch(`https://henrywang.xyz/captcha/verify?phone=${phone}&captcha=${captcha}`);
   return await resp.json();
 }
