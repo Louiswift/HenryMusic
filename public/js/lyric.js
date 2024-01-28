3c94229567eed92dbfc4c080cbc1430d59c25daa
王强 <jack2392228720@gmail.com>
2024-01-29 01:00:13 +0800

    fix: 删除无用代码

--- public/js/lyric.js
+++ public/js/lyric.js
@@ -1,16 +1,13 @@
 const rightTop = document.querySelector(".rightTop");
-const nav = document.querySelector(".nav-wrap");
 const lyricWrap = document.querySelector("#lyric-wrap");
 
 pic.addEventListener('click', () => {
     if (rightTop.style.display == "block") {
-        nav.style.display = "none";
         rightTop.style.display = "none";
       rightTop.style.opacity = "0";
       lyricWrap.style.opacity = "1";
         lyricWrap.style.display = "block";
     }else{
-        nav.style.display = "flex";
         rightTop.style.display = "block";
       rightTop.style.opacity = "1";
       lyricWrap.style.opacity = "0";
