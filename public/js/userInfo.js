47362fa3a353dd9f78d89bf26a17118f5d862630
王强 <jack2392228720@gmail.com>
2024-01-31 18:32:08 +0800

    feat：登录显示您是否为Vip & 减少重复代码 &分类您的歌单

--- public/js/userInfo.js
+++ public/js/userInfo.js
@@ -1,11 +1,12 @@
 // 获取用户头像dom
 const headSculpture = document.querySelector('.headSculpture');
 const userInfoWrap = document.querySelector('.userInfoWrap');
+const vip = document.querySelector('#vip');
+const userlevel = document.querySelector('.userlevel');
 
 // 获取用户详细信息中的用户头像dom
 const userInfoHeadSculpture = document.querySelector('.userInfoHeadSculpture');
 const username = document.querySelector('.username');
-// const userInfo = document.querySelector('.userInfo');
 
 // 点击头像显示用户信息下拉列表
 headSculpture.addEventListener('click', function () {
@@ -19,54 +20,50 @@ headSculpture.addEventListener('click', function () {
     }
 });
 
+// 获取登录信息
 const user = JSON.parse(localStorage.getItem('user'));
-if (user && user.account !== null) {
-    headSculpture.src = user.profile.avatarUrl;
-    userInfoHeadSculpture.src = user.profile.avatarUrl;
-    username.innerText = user.profile.nickname;
 
+// 如果登录则添加用户信息以及歌单等 否则右上角显示登录按钮
+if (user && user.account !== null) {
     getUserDetails(user.account.id).then(resp => {
         console.log(resp)
+        headSculpture.src = resp.profile.avatarUrl;
+        userInfoHeadSculpture.src = resp.profile.avatarUrl;
+        username.textContent = resp.profile.nickname;
+        userlevel.textContent = `LV${resp.bindings.level}`;
+        if(resp.profile.vipType == 11){
+            vip.src = 'img/vip.svg';
+        }else{
+            vip.src = 'img/普通用户.svg';
+        }
     });
 
-    // 获取用户信息中的歌单dom
+    // 获取用户信息中的歌单
     getUserPlaylists(user.account.id).then(async resp => {
         const createdPlaylist = document.querySelector('#createdPlaylist');
+        const collectPlaylists = document.querySelector('#collectPlaylists');
         const playlist = resp.playlist;
-        console.log(playlist)
-        for (let i = 0; i < playlist.length - 2; i++) {
-            // 创建dom
-            const li = document.createElement('li');
-            const a = document.createElement('a');
-            const img = document.createElement('img');
-            const div = document.createElement('div');
 
-            // 给dom赋值class & 值
-            div.classList = 'txtplaylistName f-thide';
-            img.src = playlist[i].coverImgUrl;
-            div.innerText = playlist[i].name;
-            li.setAttribute("data-song-id", playlist[i].id);
-
-            // 将dom拼装
-            a.appendChild(img);
-            a.appendChild(div);
-            li.appendChild(a);
-            createdPlaylist.appendChild(li);
+        for (let i = 0; i < playlist.length; i++) {
+            if (playlist[i].subscribed == true) {
+                createYourPlaylist(collectPlaylists);
+            } else {
+                createYourPlaylist(createdPlaylist);
+            }
         }
         clickOnPlaylist(createdPlaylist);
     })
-
     getUserInfoAndPlaylist().then(resp => {
         console.log(resp)
     });
 
 
 } else {
-    headSculpture.src = 'img/登录.svg';
-    headSculpture.addEventListener('click', () => {
-        window.location.href = 'login.html';
-        userInfoHeadSculpture.src = 'https://nimg.ws.126.net/?url=http%3A%2F%2Fdingyue.ws.126.net%2F2023%2F0406%2F5da2c82ej00rsof5j0038d200et00djg005i0050.jpg&thumbnail=660x2147483647&quality=80&type=jpg';
-        username.innerText = "游客";
-        userInfoWrap.style.display = 'none';
-    })
+        headSculpture.src = 'img/登录.svg';
+        headSculpture.addEventListener('click', () => {
+            window.location.href = 'login.html';
+            userInfoHeadSculpture.src = 'https://nimg.ws.126.net/?url=http%3A%2F%2Fdingyue.ws.126.net%2F2023%2F0406%2F5da2c82ej00rsof5j0038d200et00djg005i0050.jpg&thumbnail=660x2147483647&quality=80&type=jpg';
+            username.innerText = "游客";
+            userInfoWrap.style.display = 'none';
+        })
 }
