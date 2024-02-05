b44a35e91fef01f9837ee157fc186f2ca4ef9837
王强 <jack2392228720@gmail.com>
2024-02-06 01:12:54 +0800

    fix：歌词功能独立化

--- public/js/functions.js
+++ public/js/functions.js
@@ -394,6 +394,58 @@ async function setSongInfo(songId) {
                 console.log(resp.message);
             }
         });
+
+        await getsongLyric(songId).then(resp => {
+            lyric.innerText = '';
+            let lrc = resp.lrc.lyric;
+            lrcData = lrc
+                .split('\n')
+                .filter((s) => s)
+                .map((s) => {
+                    const parts = s.split(']');
+                    const timeParts = parts[0].replace('[', '').split(':');
+                    return {
+                        time: +timeParts[0] * 60 + +timeParts[1],
+                        words: parts[1],
+                    };
+                });
+            lyric.innerHTML = lrcData.map((lrc) => `<li>${lrc.words}</li>`).join('');
+            audio.addEventListener("timeupdate", () => {
+                setStatus(audio.currentTime);
+            });
+    
+            function setStatus(time) {
+                time += 0.5;
+    
+                const activeLi = document.querySelector('.active');
+                activeLi && activeLi.classList.remove('active');
+    
+                const index = lrcData.findIndex((lrc) => lrc.time > time) - 1;
+                if (index < 0) {
+                    return;
+                }
+                lyric.children[index].classList.add('active');
+    
+                if (lyricWrap.style.display == "block") {
+                    // 滚动
+                    if (activeLi && activeLi.innerHTML !== '') {
+                        const size = {
+                            liHeight: activeLi.offsetHeight,
+                            containerHeight: lyricWrap.offsetHeight
+                        };
+                        if (size.containerHeight !== Infinity) {
+                            let top = size.liHeight * index + size.liHeight / 2 - size.containerHeight / 2;
+                            top = -top;
+                            if (top > 0) {
+                                top = 0;
+                            }
+                            lyric.style.transform = `translate3d(0, ${top}px, 0)`;
+                            lyric.style.transition = `2s`;
+                        }
+                    }
+                }
+            }
+        });
     } else {
         console.log('没有获取到歌曲Id')
     }
@@ -412,59 +464,6 @@ async function playMain() {
 
     localStorage.setItem('play', '1');
     let playingSongId = playList[order].id;
-
-    await getsongLyric(playingSongId).then(resp => {
-        lyric.innerText = '';
-        let lrc = resp.lrc.lyric;
-        lrcData = lrc
-            .split('\n')
-            .filter((s) => s)
-            .map((s) => {
-                const parts = s.split(']');
-                const timeParts = parts[0].replace('[', '').split(':');
-                return {
-                    time: +timeParts[0] * 60 + +timeParts[1],
-                    words: parts[1],
-                };
-            });
-        lyric.innerHTML = lrcData.map((lrc) => `<li>${lrc.words}</li>`).join('');
-        audio.addEventListener("timeupdate", () => {
-            setStatus(audio.currentTime);
-        });
-
-        function setStatus(time) {
-            time += 0.5;
-
-            const activeLi = document.querySelector('.active');
-            activeLi && activeLi.classList.remove('active');
-
-            const index = lrcData.findIndex((lrc) => lrc.time > time) - 1;
-            if (index < 0) {
-                return;
-            }
-            lyric.children[index].classList.add('active');
-
-            if (lyricWrap.style.display == "block") {
-                // 滚动
-                if (activeLi && activeLi.innerHTML !== '') {
-                    const size = {
-                        liHeight: activeLi.offsetHeight,
-                        containerHeight: lyricWrap.offsetHeight
-                    };
-                    if (size.containerHeight !== Infinity) {
-                        let top = size.liHeight * index + size.liHeight / 2 - size.containerHeight / 2;
-                        top = -top;
-                        if (top > 0) {
-                            top = 0;
-                        }
-                        lyric.style.transform = `translate3d(0, ${top}px, 0)`;
-                        lyric.style.transition = `2s`;
-                    }
-                }
-            }
-        }
-    });
-
 
     if (playingSongId !== Number(songId)) {
         await setSongInfo(playList[order].id);
