231118a6059c3bea6c2baaa58c8350497436e92a
王强 <jack2392228720@gmail.com>
2024-02-06 00:02:38 +0800

    fix：更新歌单歌曲数量

--- public/js/playlistList.js
+++ public/js/playlistList.js
@@ -1,9 +1,11 @@
 let dataId = getParameterByName("id");
 
-getPlaylistsDetail(dataId).then(resp => {
+getPlaylistsDetail(dataId).then(async resp => {
     const listImg = document.querySelector(".songlist-pic img");
     const listName = document.querySelector(".song-list-name");
     const bgImgTitle = document.querySelector('.bgImgTitle');
+    const ul = document.querySelector("#list");
+    const similarPlaylistsUl = document.querySelector('#similarPlaylistsUl');
 
     const { playlist } = resp;
     const { coverImgUrl, name } = playlist || {};
@@ -12,12 +14,8 @@ getPlaylistsDetail(dataId).then(resp => {
     listName.innerText = name;
     title.innerText = name;
 
-});
-
-getAllsongsOnThePlaylist(dataId).then(async resp => {
-    const ul = document.querySelector("#list");
-    const similarPlaylistsUl = document.querySelector('#similarPlaylistsUl');
-    let songs = resp.songs;
+    let songs = resp.playlist.tracks;
+    console.log(resp.playlist.tracks)
     creatList(songs, ul);
     await addDblClickEventListener(ul);
     await settingUpViewing(songs);
@@ -28,5 +26,23 @@ getAllsongsOnThePlaylist(dataId).then(async resp => {
         const playlists = resp.playlists;
         creatSimilarSingers(playlists,similarPlaylistsUl);
         clickOnPlaylist(similarPlaylistsUl);
-    })
+    })    
+
+});
+
+getAllsongsOnThePlaylist(dataId).then(async resp => {
+    const ul = document.querySelector("#list");
+    const similarPlaylistsUl = document.querySelector('#similarPlaylistsUl');
+    // let songs = resp.songs;
+    // creatList(songs, ul);
+    // await addDblClickEventListener(ul);
+    // await settingUpViewing(songs);
+    // clickArnamelist(ul)
+
+    // const { firstId } = ul.dataset;
+    // similarPlaylists(firstId).then(resp => {
+    //     const playlists = resp.playlists;
+    //     creatSimilarSingers(playlists,similarPlaylistsUl);
+    //     clickOnPlaylist(similarPlaylistsUl);
+    // })
 })
