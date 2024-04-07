3f5fe51434582e5243727034212c3d33d8d10467
王强 <jack2392228720@gmail.com>
2024-04-07 19:18:46 +0800

    fix: 删除无用代码

--- public/js/playlistList.js
+++ public/js/playlistList.js
@@ -29,20 +29,3 @@ getPlaylistsDetail(dataId).then(async resp => {
         clickOnPlaylist(similarPlaylistsUl);
     })
 });
-
-getAllsongsOnThePlaylist(dataId).then(async resp => {
-    const ul = document.querySelector("#list");
-    const similarPlaylistsUl = document.querySelector('#similarPlaylistsUl');
-    // let songs = resp.songs;
-    // creatList(songs, ul);
-    // await addDblClickEventListener(ul);
-    // await settingUpViewing(songs);
-    // clickArnamelist(ul)
-
-    // const { firstId } = ul.dataset;
-    // similarPlaylists(firstId).then(resp => {
-    //     const playlists = resp.playlists;
-    //     creatSimilarSingers(playlists,similarPlaylistsUl);
-    //     clickOnPlaylist(similarPlaylistsUl);
-    // })
-})
